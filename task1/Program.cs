a80258ba315d0c83d32ee43459460b2f754be233
DESKTOP-QP6E4JG\Valtas <Kostoev-23@yandex.ru>
2022-10-25 12:32:04 +0300

    task1 complete

---
+++ task1/Program.cs
@@ -0,0 +1,48 @@
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//Разделить ввод данных, от расчета данных (Одна функция вводит данные, другая функция подсчитывает количество положительных)
+
+//0, 7, 8, -2, -2-> 2
+
+//1, -7, 567, 89, 223-> 3
+
+
+
+void PrintArray(int[] arr)
+{
+    int length = arr.Length;
+    int index = 0;
+    Console.Write('[');
+    while (index < length)
+    {
+        Console.Write($" {arr[index]}, ");
+        index++;
+    }
+    Console.Write(']');
+}
+
+int[] NumsIntake(string nums)
+{
+    int[] arr = Array.ConvertAll(nums.Trim().Split(' '), Convert.ToInt32);
+    return arr;
+}
+
+int AboveZero(int[] arr)
+{
+    int counter = 0;
+    for (int i = 0; i<arr.Length; i++)
+    {
+        if (arr[i] != 0 && arr[i] > 0)
+        {
+            counter++;
+        }
+    }
+    
+    return counter;
+}
+
+Console.WriteLine("Введите любое количество чисел через пробел");
+
+int[] array = NumsIntake(Console.ReadLine() ?? "0");
+
+PrintArray(array);
+Console.Write($" -> Больше нуля {AboveZero(array)} чисел");
